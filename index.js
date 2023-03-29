0a4aa8561d63df577dc5413d7a94f64366f983c2
gabrielcbritto <gabrielcbritto@gmail.com>
2023-03-28 21:34:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+
+
+const orders = []
+
+const checkIdTrue = (request, response, next) => {
+    const { id } = request.params
+
+    const index = orders.findIndex(od => od.id === id)
+
+    if (index < 0){
+        return response.status(404).json({message:"Order not Found"})
+    }
+
+    request.orderIndex = index
+    request.orderId = id
+
+    next()
+}
+
+app.post('/order', (request, response) => {
+    const { order, clientName, price} = request.body
+    const orderF = { id: uuid.v4(), order, clientName, price, status: "Em prep"}
+    orders.push(orderF)
+    return response.json(orderF)
+})
+
+
+
+app.get ('/order', (request, response) => response.json(orders))
+
+app.put('/order/:id', checkIdTrue, (request, response)=> {
+    const id = request.orderId
+
+    const {order, clientName, price} = request.body
+
+    const index = request.orderIndex
+
+    const updatedOrder = {id, order, clientName, price, status:"Em prep"}
+
+    orders[index] = updatedOrder
+
+    return response.json(updatedOrder)
+})
+
+
+app.delete('/order/:id', checkIdTrue, (request, response) => {
+    const index = request.orderIndex
+    orders.splice(index, 1)
+    return response.status(201).json({message:"Order Deleted"})
+})
+
+app.get ('/order/:id', checkIdTrue, (request, response)=> {
+    const index = request.orderIndex
+    console.log(orders[index])
+    return response.json(orders[index])
+})
+
+app.patch ('/order/:id', checkIdTrue, (request, response) => {
+    const orderChanges = orders[request.orderIndex]
+    orderChanges.status = "Pronto"
+    console.log(orderChanges)
+    return response.json(orderChanges) 
+})
+
+app.listen(port, () => {
+    console.log(`👾☠🤖🐱‍🏍Server is running on port ${port}`)
+})
+
+
